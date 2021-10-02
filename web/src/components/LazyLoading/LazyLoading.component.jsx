f8aefcb288d8f69639d46da48779a05b019d90f3
DhananjayaSLIIT <it19127538@my.sliit.lk>
2021-10-02 21:59:21 +0530

    side bar pull request Dhananjaya

---
+++ web/src/components/LazyLoading/LazyLoading.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LazyLoadingComponent = () => {
+    return(
+        <div className="lazy_container">
+
+        </div>
+    );
+}
+export default LazyLoadingComponent;
---
+++ web/src/components/SupplierQuotation/components/TopVendors.component.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class TopVendorsComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            topVendors:[]
+        }
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default TopVendorsComponent;
